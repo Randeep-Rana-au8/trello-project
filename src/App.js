b7ecf6550a3413eb292225710ccebb35418d0bb8
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2020-11-26 23:42:22 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import _ from "lodash";
